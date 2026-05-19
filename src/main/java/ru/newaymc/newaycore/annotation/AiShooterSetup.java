28f5b010627b49b1d9b1b5d179c99c8c28dfe2d6
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-05-19 18:04:24 +0300

    Added annotation AiShooterSetup

---
+++ src/main/java/ru/newaymc/newaycore/annotation/AiShooterSetup.java
@@ -0,0 +1,16 @@
+package ru.newaymc.newaycore.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AiShooterSetup {
+    String aiType() default "standard";
+    double ammunition() default 30;
+    double damage() default 3;
+    double recoveryTime() default 20;
+    double speed() default 3;
+}
