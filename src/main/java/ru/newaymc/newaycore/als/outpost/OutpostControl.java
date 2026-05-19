9ad381f79646ecdce063c60c91d10bbbc280f0c0
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-05-19 18:14:50 +0300

    Remove EliteShooterEntity

--- src/main/java/ru/newaymc/newaycore/als/outpost/OutpostControl.java
+++ src/main/java/ru/newaymc/newaycore/als/outpost/OutpostControl.java
@@ -14,14 +14,14 @@ import ru.newaymc.newaycore.init.ModEntities;
 @Deprecated(forRemoval = true)
 public class OutpostControl {
     public static void execute(LevelAccessor world, double x, double y, double z) {
-        boolean UnderSecurity = false;
-        boolean HaveSquad = false;
-        Entity OutpostSecurity = null;
-        Entity cmd = null;
-        double rnd = 0;
-        if (world.getEntitiesOfClass(ShooterAiEntity.class, new AABB(Vec3.ZERO, Vec3.ZERO).move(new Vec3(x, y, z)).inflate(16 / 2d), e -> true).isEmpty() && world.dayTime() == 1000) {
-            UnderSecurity = false;
-        }
+//        boolean UnderSecurity = false;
+//        boolean HaveSquad = false;
+//        Entity OutpostSecurity = null;
+//        Entity cmd = null;
+//        double rnd = 0;
+//        if (world.getEntitiesOfClass(ShooterAiEntity.class, new AABB(Vec3.ZERO, Vec3.ZERO).move(new Vec3(x, y, z)).inflate(16 / 2d), e -> true).isEmpty() && world.dayTime() == 1000) {
+//            UnderSecurity = false;
+//        }
 //        if (!UnderSecurity) {
 //            for (int index0 = 0; index0 < (int) getBlockNBTNumber(world, BlockPos.containing(x, y, z), "security-count"); index0++) {
 //                OutpostSecurity = world instanceof ServerLevel _level3 ? ModEntities.STANDART_SHOOTER_ENTITY.get().spawn(_level3, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED) : null;
@@ -30,16 +30,16 @@ public class OutpostControl {
 //            }
 //            UnderSecurity = true;
 //        }
-        if (!HaveSquad && world.dayTime() == 1000) {
-            cmd = world instanceof ServerLevel _level6 ? ModEntities.ELITE_SHOOTER_ENTITY.get().spawn(_level6, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED) : null;
-            for (int index1 = 0; index1 < 4; index1++) {
-                if (world instanceof ServerLevel _level) {
-                    Entity entityToSpawn = ModEntities.STANDART_SHOOTER_ENTITY.get().spawn(_level, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED);
-                    if (entityToSpawn != null) {
-                    }
-                }
-            }
-        }
+//        if (!HaveSquad && world.dayTime() == 1000) {
+//            cmd = world instanceof ServerLevel _level6 ? ModEntities.ELITE_SHOOTER_ENTITY.get().spawn(_level6, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED) : null;
+//            for (int index1 = 0; index1 < 4; index1++) {
+//                if (world instanceof ServerLevel _level) {
+//                    Entity entityToSpawn = ModEntities.STANDART_SHOOTER_ENTITY.get().spawn(_level, BlockPos.containing(x, y, z), MobSpawnType.MOB_SUMMONED);
+//                    if (entityToSpawn != null) {
+//                    }
+//                }
+//            }
+//        }
     }
 
     private static double getBlockNBTNumber(LevelAccessor world, BlockPos pos, String tag) {
