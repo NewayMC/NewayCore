6a0a4dd4c22bcd5cb01cedabd3523e571e002a12
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-07-15 23:28:37 +0300

    Switched from serializable data to Memory

---
+++ src/main/java/ru/newaymc/newaycore/ai/objects/Memory.java
@@ -0,0 +1,31 @@
+package ru.newaymc.newaycore.ai.objects;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.phys.Vec3;
+import ru.newaymc.newaycore.gun.GunSetup;
+
+@Getter
+@Setter
+public class Memory {
+    private int ticks;
+    // -- Entity --
+    private PathfinderMob mob;
+    private Cover currentCover = null;
+    private boolean coverStatus = false;
+    private boolean borderPatrol = true;
+    private GunSetup.Type type;
+    // -- Target --
+    private LivingEntity target = null;
+    private Vec3 lastTargetPos = null;
+    private boolean seeTarget = false;
+    private long lastSeenTime = 0;
+    // -- Faction --
+    private String role = null;
+
+    public Memory(PathfinderMob mob) {
+        this.mob = mob;
+    }
+}
