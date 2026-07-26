7cd01262926bb2f70b7a82dac0ee1dcf6a3444b9
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-07-27 00:00:37 +0300

    Mass cleanup

--- src/main/java/ru/newaymc/newaycore/ai/utils/Interruptible.java
+++
@@ -1,7 +0,0 @@
-package ru.newaymc.newaycore.ai.utils;
-
-import ru.newaymc.newaycore.ai.objects.Memory;
-
-public interface Interruptible {
-    void onInterrupt(Memory memory);
-}
--- src/main/java/ru/newaymc/newaycore/ai/utils/Status.java
+++
@@ -1,7 +0,0 @@
-package ru.newaymc.newaycore.ai.utils;
-
-public enum Status {
-    SUCCESS,
-    FAILURE,
-    RUNNING
-}
