22e0371975f3db19345e0e80f6f1bcc941c95a63
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-08-30 18:00:24 +0300

    Cleanup

--- src/main/java/ru/newaymc/newaycore/NewaycoreMod.java
+++ src/main/java/ru/newaymc/newaycore/NewaycoreMod.java
@@ -3,18 +3,15 @@ package ru.newaymc.newaycore;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.util.Tuple;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLPaths;
-import net.neoforged.fml.util.thread.SidedThreadGroups;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,17 +20,13 @@ import ru.newaymc.newaycore.register.*;
 import ru.newaymc.newaycore.worlds.providers.ModWorldgenProvider;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Mod("newaycore")
 public class NewaycoreMod {
     public static final String MODID = "newaycore";
     public static final String MOD_DIR = (FMLPaths.GAMEDIR.get().toString() + "/newaycore/");
 
-    private static final Collection<Tuple<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
     public static HolderLookup.Provider provider;
 
     public static final Logger LOGGER = LogManager.getLogger(MODID);
@@ -47,18 +40,6 @@ public class NewaycoreMod {
         ModEntities.REGISTRY.register(modEventBus);
 
         modEventBus.addListener(this::gatherData);
-    }
-
-    @SubscribeEvent
-    public void tick(ServerTickEvent.Post event) {
-        List<Tuple<Runnable, Integer>> actions = new ArrayList<>();
-        workQueue.forEach(work -> {
-            work.setB(work.getB() - 1);
-            if (work.getB() == 0)
-                actions.add(work);
-        });
-        actions.forEach(e -> e.getA().run());
-        workQueue.removeAll(actions);
     }
 
     @SubscribeEvent
@@ -77,12 +58,6 @@ public class NewaycoreMod {
     private void prepareModDirectories() {
         ZstdFileCompressor.prepareDirectory(MOD_DIR);
         new File(MOD_DIR + "/saves/data/").mkdirs();
-    }
-
-    public static void queueServerWork(int tick, Runnable action) {
-        if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER) {
-            workQueue.add(new Tuple<>(action, tick));
-        }
     }
 
     @EventBusSubscriber
