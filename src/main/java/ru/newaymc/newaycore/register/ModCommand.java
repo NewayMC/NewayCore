7cdbd49c8a9a5710b7dcfb55e6c0603eea87fa53
NW-_-Boog55_ <timcenkobogdan17@gmail.com>
2026-09-09 20:43:29 +0300

    Update DimensionLoader

---
+++ src/main/java/ru/newaymc/newaycore/register/ModCommand.java
@@ -0,0 +1,60 @@
+package ru.newaymc.newaycore.register;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.MessageArgument;
+import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+
+import ru.newaymc.newaycore.worlds.DimensionLoader;
+import ru.newaymc.newaycore.worlds.build.WorldRegister;
+import ru.newaymc.newaycore.worlds.build.WorldTemplate;
+
+import java.util.Optional;
+
+@EventBusSubscriber
+public class ModCommand {
+
+    @SubscribeEvent
+    public static void registerCommand(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+
+        dispatcher.register(Commands.literal("newaycore").requires(s -> s.hasPermission(4))
+                .then(Commands.literal("prepare").then(Commands.argument("worldId", MessageArgument.message()).executes(ModCommand::prepare)))
+                .then(Commands.literal("load").then(Commands.argument("worldId", MessageArgument.message()).executes(ModCommand::load)))
+        );
+    }
+
+    private static int prepare(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        String dimensionPath = MessageArgument.getMessage(context, "worldId").getString();
+
+        Optional<WorldTemplate> worldTemplate = WorldRegister.findDimension(dimensionPath);
+        if (worldTemplate.isEmpty()) {
+            context.getSource().sendFailure(Component.literal("Error while dimension preparation"));
+            return 0;
+        }
+        DimensionLoader.prepareDimension(worldTemplate.get().getDimensionId());
+
+        context.getSource().sendSuccess(() -> Component.literal("Preparing..."), false);
+        return 1;
+    }
+
+    private static int load(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        String dimensionPath = MessageArgument.getMessage(context, "worldId").getString();
+
+        Optional<WorldTemplate> worldTemplate = WorldRegister.findDimension(dimensionPath);
+        if (worldTemplate.isEmpty()) {
+            context.getSource().sendFailure(Component.literal("Error while dimension loading"));
+            return 0;
+        }
+        DimensionLoader.loadDimension(worldTemplate.get().getDimensionId(), false);
+
+        context.getSource().sendSuccess(() -> Component.literal("Loading..."), false);
+        return 1;
+    }
+}
